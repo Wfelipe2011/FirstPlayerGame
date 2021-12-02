f57c5a7c15c9b47a12596c910358a337396ce57a
wilson <wilson.silva@tegra.com.br>
2021-12-02 07:40:32 -0300

    iniciando typescript no projeto

--- public/create-game.js
+++ public/create-game.js
@@ -1,4 +1,4 @@
-export function createGame() {
+module.exports = function createGame() {
 
     const state = {
         players: {},
---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import createGame from "../public/create-game.js";
+
+const app = express();
+const server = createServer(app);
+const sockets = new Server(server);
+
+app.use(express.static("public"));
+
+const game = createGame();
+let gameAddFruit = null;
+
+game.subscribe((command) => {
+  // console.log(`> Emitting ${command.type}`);
+  sockets.emit(command.type, command);
+});
+
+sockets.on("connection", (socket) => {
+  const playerId = socket.id;
+  // console.log(`Player connected on Server ${playerId}`)
+
+  if (game.gameOver()) gameAddFruit = game.start();
+
+  game.addPlayer({ playerId });
+
+  socket.on("disconnect", () => {
+    game.removePlayer({ playerId });
+    game.unSubscribe();
+
+    if (game.gameOver()) game.stop(gameAddFruit);
+  });
+
+  socket.emit("setup", game.state);
+
+  socket.on("move-player", (command) => {
+    // console.log(command);
+    command.playerId = playerId;
+    command.type = "move-player";
+
+    game.movePlayer(command);
+  });
+});
+
+server.listen(process.env.PORT || 3001, () => {
+  console.log("> Sever listening on port: 3000");
+});
