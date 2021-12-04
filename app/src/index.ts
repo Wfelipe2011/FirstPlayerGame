7c240d913978694474d3ae7b3d0472ea5cc4e553
wilson <wilson.silva@tegra.com.br>
2021-12-04 07:24:47 -0300

    gabis para rodar em dev

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,13 +1,13 @@
-import { Game } from "./module/game.js";
-import { createGame } from "./core/create-game.js";
-import { SetupExpress } from "./module/infra/setup-express.js";
+import { createGame } from "./core/create-game";
+import { Game } from "./module/game";
+import { SetupExpress } from "./module/infra/setup-express";
 
 const game = createGame();
 const setupExpress = new SetupExpress();
 const newGame = new Game(game, setupExpress.sockets);
 
 class SetupServer {
-  static start(setupExpress: SetupExpress, newGame: Game) {
+  static start(setupExpress, newGame) {
     newGame.start();
     setupExpress.start();
   }
---
+++ dist/src/index.js
@@ -0,0 +1,14 @@
+import { createGame } from "./core/create-game";
+import { Game } from "./module/game";
+import { SetupExpress } from "./module/infra/setup-express";
+const game = createGame();
+const setupExpress = new SetupExpress();
+const newGame = new Game(game, setupExpress.sockets);
+class SetupServer {
+    static start(setupExpress, newGame) {
+        newGame.start();
+        setupExpress.start();
+    }
+}
+console.log("teste");
+SetupServer.start(setupExpress, newGame);
